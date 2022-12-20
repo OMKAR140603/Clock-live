50e219c7f01971256c6bb7d676b7a17c3372b0ba
OMKAR140603 <81954092+OMKAR140603@users.noreply.github.com>
2022-12-20 20:16:56 +0530

    Live time Clock using html css js

---
+++ clock-live/main.js
@@ -0,0 +1,21 @@
+setInterval(()=>{
+    // console.log("omi");
+    d= new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+
+
+    hrotation= 30 * htime + mtime/2;
+    mrotation= 6*mtime;
+    srotation= 6*stime;
+
+
+
+    hour.style.transform = `rotate(${hrotation}deg)`
+    min.style.transform = `rotate(${mrotation}deg)`
+    sec.style.transform = `rotate(${srotation}deg)`
+   
+   
+    // console.log(date);
+},1000)
